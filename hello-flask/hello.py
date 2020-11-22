3c2ddc90d2dd8e86851d77e06c4d6ca6030c5e9d
Nick Hand <nicholas.adam.hand@gmail.com>
2020-11-22 16:38:44 -0500

    update hello.py

--- hello-flask/hello.py
+++ hello-flask/hello.py
@@ -7,21 +7,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     """
-    This function runs when the "/" route (the home page of 
+    This function runs when the "/" route (the home page of
     your application) is requested by a browser
     """
-    return "My name is Nick"
-
-
-@app.route("/test")
-def hello_test():
-    """
-    This function runs when the "/" route (the home page of 
-    your application) is requested by a browser
-    """
-    return "Hello, this is a test"
+    return "Hello, World!"
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-
